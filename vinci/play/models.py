e21178358c01d4a33b941cf8dc1a165113113126
David Sykes <david.sykes@octoenergy.com>
2016-12-28 16:49:44 +0000

    correct answer tracking added

--- vinci/play/models.py
+++ vinci/play/models.py
@@ -9,7 +9,8 @@ class Trail(models.Model):
 
 
 class Question(models.Model):
-    trail = models.ForeignKey(Trail, null=True, on_delete=models.SET_NULL, related_name='questions')
+    trail = models.ForeignKey(Trail, null=True, on_delete=models.SET_NULL,
+                              related_name='questions')
     question = models.CharField(max_length=1000, null=False)
     location_answer = models.BooleanField(default=False)
     answer = models.CharField(max_length=1000, null=True)
@@ -19,15 +20,16 @@ class Question(models.Model):
     answer_longitude = models.FloatField(null=True)
 
 
-class CookieTracker(models.Model):
-    trail = models.ForeignKey(Trail, null=True, on_delete=models.SET_NULL, related_name='cookies')
+class TrailCookie(models.Model):
+    trail = models.ForeignKey(Trail, null=True, on_delete=models.SET_NULL,
+                              related_name='trail_cookies')
     uid = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class CookieQuestionTracker(models.Model):
-    cookie_tracker = models.ForeignKey(CookieTracker, null=True,
-                                       on_delete=models.SET_NULL, related_name='cookie_questions')
-    question = models.ForeignKey(CookieTracker, null=True,
-                                       on_delete=models.SET_NULL, related_name='cookies')
+class CorrectQuestion(models.Model):
+    trail_cookie = models.ForeignKey(TrailCookie, null=True,
+                                       on_delete=models.SET_NULL, related_name='correct_questions')
+    question = models.ForeignKey(Question, null=True,
+                                       on_delete=models.SET_NULL, related_name='correct_questions')
     created_at = models.DateTimeField(auto_now_add=True)
--- vinci/play/views.py
+++ vinci/play/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Trail, Question, CookieTracker, CookieQuestionTracker
+from django.shortcuts import render, HttpResponse
+from .models import Trail, Question, TrailCookie, CorrectQuestion
 from django.core import serializers
 import json
 
@@ -9,22 +9,37 @@ def index(request):
     trail_id = int(request.GET.get('id'))
     trail = Trail.objects.get(pk=trail_id)
     questions = trail.questions.all()
-    question_json = serializers.serialize("json", questions)
+    question_json = json.loads(serializers.serialize("json", questions))
+    
     """Check if they have a cookie for this trail and create one if not"""
-    cookie = request.COOKIES.get(str(trail.uid))
-    if cookie:
-        cookie_tracker = CookieTracker.objects.filter(uid = cookie).last()
+    cookie_key = request.COOKIES.get(str(trail.uid))
+    if cookie_key:
+        trail_cookie = TrailCookie.objects.filter(uid = cookie_key).last()
     else:
-        cookie_tracker = CookieTracker(trail=trail)
-        cookie_tracker.save()
-
+        trail_cookie = TrailCookie(trail=trail)
+        trail_cookie.save()
 
     """Add a field in question_json for each question if it is completed or not"""
-
-
-    response = render(request, 'index.html', {'trail':trail, 'question_json':question_json})
-    response.set_cookie(str(trail.uid), value=str(cookie_tracker.uid), max_age=1000000)
+    correct_questions = trail_cookie.correct_questions.all()
+    correct_question_ids = [cq.question.id for cq in correct_questions]
+    for q in question_json:
+        if q['pk'] in correct_question_ids:
+            q['correct'] = True
+        else:
+            q['correct'] = False
+    question_json_string = json.dumps(question_json)
+    response = render(request, 'index.html', {'trail':trail, 'question_json':question_json_string})
+    response.set_cookie(str(trail.uid), value=str(trail_cookie.uid), max_age=1000000)
 
     return response
 
 
+def correct_answer(request):
+    question_id =  int(request.GET.get('id'))
+    question = Question.objects.get(pk=question_id)
+    trail = question.trail
+    cookie_key = request.COOKIES.get(str(trail.uid))
+    trail_cookie = TrailCookie.objects.filter(uid = cookie_key).last()
+    c = CorrectQuestion(trail_cookie=trail_cookie, question=question)
+    c.save()
+    return HttpResponse('{}', content_type="application/json")
