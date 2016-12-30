416f89805a03a32c1e2c95fba04f8f1e0a312aba
David Sykes <david.sykes@octoenergy.com>
2016-12-30 20:46:20 +0000

    correct answer returns question_json

--- vinci/play/models.py
+++ vinci/play/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import uuid
+from django.core import serializers
+import json
 # Create your models here.
 
 class Trail(models.Model):
@@ -26,6 +28,22 @@ class TrailCookie(models.Model):
     uid = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def return_question_json_string(self):
+        questions = self.trail.questions.all()
+        correct_questions = self.correct_questions.all()
+        correct_question_ids = [cq.question.id for cq in correct_questions]
+        question_json = json.loads(serializers.serialize("json", questions))
+        for q in question_json:
+            if q['pk'] in correct_question_ids:
+                q['fields']['correct'] = True
+                q['fields']['discovered'] = True
+            else:
+                q['fields']['correct'] = False
+                q['fields']['discovered'] = False
+        print(question_json)
+        question_json_string = json.dumps(question_json)
+        return question_json_string
+
 
 class CorrectQuestion(models.Model):
     trail_cookie = models.ForeignKey(TrailCookie, null=True,
@@ -33,3 +51,4 @@ class CorrectQuestion(models.Model):
     question = models.ForeignKey(Question, null=True,
                                        on_delete=models.SET_NULL, related_name='correct_questions')
     created_at = models.DateTimeField(auto_now_add=True)
+
